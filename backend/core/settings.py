ee1c3051796d8997b9924faab3471248a212bec2
Showlden <kalybekov.shahnur1529@gmail.com>
2025-05-05 13:35:02 +0600

    cors fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -43,8 +43,11 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'core.urls'
 
